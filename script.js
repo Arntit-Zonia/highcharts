2bd629e425747f89885a5d3132772cd343b60a92
Arntit-Zonia <48775524+Arntit-Zonia@users.noreply.github.com>
2019-08-02 14:18:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,7 @@ let myChart = Highcharts.chart("container0", {
             text: "Fruit eaten"
         }
     },
-    series: [
-        {
+    series: [{
             name: "John",
             data: [1, 0, 4]
         },
@@ -39,7 +38,7 @@ let options = {
     }]
 }
 
-$(document).ready(()=> {
+$(document).ready(() => {
     let chart = new Highcharts.chart(options);
 });
 
@@ -60,21 +59,56 @@ $(() => {
             }
         },
         title: { //sets title for nested objects
-            text: "I'm learning"
+            text: "I'm learning",
+            style: {
+                color: "red"
+            }
+        },
+        subtitle: {
+            text: "Sugoi"
         },
         borderWidth: 2,
         plotBackground: "rgba(255, 255, 255, .1)",
         plotShadow: true,
         plotBorderWidth: 1
     });
-    let chart1 = new Highcharts.chart("container2", {
+
+    let charteri = new Highcharts.chart("container2", {
         xAxis: {
+            title: {
+                text: "Such Data, Much Wow"
+            },
             type: "datetime"
         },
-        series: [
-            {
+        yAxis: [{
+            title: {
+                text: "Brilhante"
+            },
+            title: {
+                text: "Opaco",
+            },
+            labels: {
+                formatter: function () {
+                    return this.value + " %"
+                }
+            },
+            type: "logarithmic"
+        }],
+        series: [{
                 name: "charteri",
                 data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+                pointStart: Date.UTC(210, 0, 1),
+                pointInterval: 3600 * 1000 // one hour
+            },
+            {
+                name: "charteri2",
+                data: [29.9, 40, {y:200, marker: {fillColor: "green", radius: 8}}, 160, 144.0, 176.0, 63.6, 200, {y: 516.4, color: "red"}, 158.1, 90.6, 90.4],
+                pointStart: Date.UTC(210, 0, 1),
+                pointInterval: 3600 * 1000 // one hour
+            },
+            {
+                name: "charteri3",
+                data: [129.9, 140, 80, 60, 170.0, 76.0, 40.6, 100, 216.4, 58.1, 200.6, 40.4],
                 pointStart: Date.UTC(210, 0, 1),
                 pointInterval: 3600 * 1000 // one hour
             }
@@ -88,13 +122,11 @@ $(() => {
         xAxis: {
             type: "datetime"
         },
-        series: [
-            {
-                name: "charteroni",
-                data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
-                pointStart: Date.UTC(2010, 0, 1),
-                pointInterval: 3600 * 1000 // one hour
-            }
-        ]
+        series: [{
+            name: "charteroni",
+            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+            pointStart: Date.UTC(2010, 0, 1),
+            pointInterval: 3600 * 1000 // one hour
+        }]
     });
 });
